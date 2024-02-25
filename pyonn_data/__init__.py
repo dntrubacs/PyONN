57252a35f3a843546cfc82c81aef657941bb3757
Daniel-Iosif <dntrubacs@gmail.com>
2024-02-25 15:55:01 +0000

    feat: created pyonn_data module

---
+++ pyonn_data/__init__.py
@@ -0,0 +1,4 @@
+"""
+Created by Daniel-Iosif Trubacs on 25 February 2024. The purpose of these
+modules are to create utils modules to process data.
+"""
---
+++ pyonn_data/datasets.py
@@ -0,0 +1,3 @@
+""" Created by Daniel-Iosif Trubacs 25 February 2024. The purpose fo this
+module is to process images datasets so they can be feed into an optical
+neural network."""
